a89e55d8a6bad7aa30678ac8834a73bddee9c124
tgavrel <tomas.gavrel@hotmail.fr>
2021-07-13 12:25:12 +0200

    Updated card styles :lipstick:

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 // import logo from './logo.svg';
 import "./App.css";
 
+// Router-dom import
 import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
+// Views imports
 import Accueil from "./views/Accueil";
 import Recherche from "./views/Recherche";
 import Favoris from "./views/Favoris";
+
+// Bootstrap imports
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar } from "react-bootstrap";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <header>
-          <NavLink className="nav-link" to="/" exact>
-            Accueil
-          </NavLink>
-          <NavLink className="nav-link" to="/recherche" exact>
-            Recherche
-          </NavLink>
-          <NavLink className="nav-link" to="/favoris" exact>
-            Favoris
-          </NavLink>
-        </header>
+        <Container>
+          <header className="mb-5">
+            <Navbar bg="dark" variant="dark">
+              <NavLink className="nav-link" activeStyle={{ color: 'cyan' }} to="/" exact>
+                Accueil
+              </NavLink>
+              <NavLink className="nav-link" activeStyle={{ color: 'cyan' }} to="/recherche" exact>
+                Recherche
+              </NavLink>
+              <NavLink className="nav-link" activeStyle={{ color: 'cyan' }} to="/favoris" exact>
+                Favoris
+              </NavLink>
+            </Navbar>
+          </header>
 
-        {/* Accueil */}
-        <Route path="/" component={Accueil} exact />
+          {/* Accueil */}
+          <Route path="/" component={Accueil} exact />
 
-        {/* Recherche */}
-        <Route path="/recherche" component={Recherche} exact />
+          {/* Recherche */}
+          <Route path="/recherche" component={Recherche} exact />
 
-        {/* Favoris */}
-        <Route path="/favoris" component={Favoris} exact />
+          {/* Favoris */}
+          <Route path="/favoris" component={Favoris} exact />
+        </Container>
       </div>
     </BrowserRouter>
   );
--- src/views/Accueil.js
+++ src/views/Accueil.js
@@ -1,5 +1,30 @@
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
+
 function Accueil() {
-  return <div>Page Accueil</div>;
+  return (
+    <div>
+      <h1>Bienvenue sur Paris Events</h1>
+      <p>L'application qui permet de rechercher en direct les prochains événements Parisiens</p>
+      <hr/>
+      <h2>Actualité</h2>
+      <p>Le dernier événement publié :</p>
+      <Row>
+        <Col lg={3} md={6} xs={12} className="mt-3">
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="info">Info</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default Accueil;
